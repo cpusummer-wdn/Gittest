de1f7681bdc8f3fc485b284497b77776f8d6798a
Danni Wang <danni_wang1997@163.com>
2022-03-25 21:15:42 +0800

    first commit

---
+++ .anaconda/navigator/.anaconda/navigator/scripts/notebook.bat
@@ -0,0 +1,4 @@
+@echo off
+chcp 936
+call C:\Users\Lvzi\anaconda3\Scripts\activate C:\Users\Lvzi\anaconda3
+jupyter-notebook >C:\Users\Lvzi\.anaconda\navigator\.anaconda\navigator\scripts\notebook-out-1.txt 2>C:\Users\Lvzi\.anaconda\navigator\.anaconda\navigator\scripts\notebook-err-1.txt
---
+++ .anaconda/navigator/scripts/anaconda-navigator-app.bat
@@ -0,0 +1,3 @@
+chcp 936
+call C:\Users\Lvzi\anaconda3\Scripts\activate C:\Users\Lvzi\anaconda3
+anaconda-navigator >C:\Users\Lvzi\.anaconda\navigator\scripts\anaconda-navigator-app-out-1.txt 2>C:\Users\Lvzi\.anaconda\navigator\scripts\anaconda-navigator-app-err-1.txt
